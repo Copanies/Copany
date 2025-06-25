29c617d5e1fc85768384777a2032013ec3971128
jinhongw <51617358+jinhongw@users.noreply.github.com>
2025-06-25 10:10:23 +0800

    add page loading view

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function HomeLoading() {
+  return (
+    <main className="flex justify-center items-center py-8">
+      <div className="text-gray-500 dark:text-gray-400">Loading...</div>
+    </main>
+  );
+}
