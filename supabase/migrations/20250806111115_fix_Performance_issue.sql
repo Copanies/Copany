a8f2926d26fa32b911bd139b9a9adfa7876b74bd
jinhongw <51617358+jinhongw@users.noreply.github.com>
2025-08-06 19:12:00 +0800

    fix supabase performance issue

---
+++ supabase/migrations/20250806111115_fix_Performance_issue.sql
@@ -0,0 +1,11 @@
+drop policy "Enable insert for authenticated users only" on "public"."copany";
+
+create policy "Enable insert for authenticated users only"
+on "public"."copany"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+
