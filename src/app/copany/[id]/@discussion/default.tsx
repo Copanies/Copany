3243386038737767167b92301e1da676083adbba
jinhongw <51617358+jinhongw@users.noreply.github.com>
2025-10-01 10:29:11 +0800

    add Parallel Route loading view

--- src/app/copany/[id]/@discussion/default.tsx
+++ src/app/copany/[id]/@discussion/default.tsx
@@ -1,3 +1,3 @@
-export default function DiscussionDefault() {
+export default function DiscussionPageDefault() {
   return null;
 }
--- src/app/copany/[id]/@issue/default.tsx
+++ src/app/copany/[id]/@issue/default.tsx
@@ -1,3 +1,3 @@
-export default function IssueDefault() {
+export default function IssuePageDefault() {
   return null;
 }
